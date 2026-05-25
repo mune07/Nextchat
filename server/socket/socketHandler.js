719e8a78b76326d7fee1ef880df62cc1818a6cfd
Muni <boyamuni113@gmail.com>
2026-05-25 16:54:15 +0530

    Fix socket undefined userId crash

--- server/socket/socketHandler.js
+++ server/socket/socketHandler.js
@@ -10,13 +10,16 @@ const socketHandler = (io) => {
     io.on('connection', async (socket) => {
         const userId = socket.handshake.query.userId;
 
-        if (userId) {
+        if (userId && userId !== 'undefined' && userId !== 'null') {
             if (!userSocketMap[userId]) {
                 userSocketMap[userId] = [];
             }
             userSocketMap[userId].push(socket.id);
 
-            await User.findByIdAndUpdate(userId, { isOnline: true, socketId: socket.id });
+            try {
+                await User.findByIdAndUpdate(userId, { isOnline: true, socketId: socket.id });
+            } catch { }
+
             io.emit('onlineUsers', Object.keys(userSocketMap));
         }
 
@@ -29,34 +32,26 @@ const socketHandler = (io) => {
         });
 
         socket.on('typing', ({ conversationId, userId: typingUserId }) => {
-            socket.to(conversationId).emit('userTyping', {
-                conversationId,
-                userId: typingUserId,
-            });
+            socket.to(conversationId).emit('userTyping', { conversationId, userId: typingUserId });
         });
 
         socket.on('stopTyping', ({ conversationId, userId: typingUserId }) => {
-            socket.to(conversationId).emit('userStopTyping', {
-                conversationId,
-                userId: typingUserId,
-            });
+            socket.to(conversationId).emit('userStopTyping', { conversationId, userId: typingUserId });
         });
 
         socket.on('disconnect', async () => {
-            if (userId && userSocketMap[userId]) {
-                userSocketMap[userId] = userSocketMap[userId].filter(
-                    (id) => id !== socket.id
-                );
-                if (userSocketMap[userId].length === 0) {
-                    delete userSocketMap[userId];
-                    await User.findByIdAndUpdate(userId, {
-                        isOnline: false,
-                        lastSeen: new Date(),
-                        socketId: '',
-                    });
+            if (userId && userId !== 'undefined' && userId !== 'null') {
+                if (userSocketMap[userId]) {
+                    userSocketMap[userId] = userSocketMap[userId].filter((id) => id !== socket.id);
+                    if (userSocketMap[userId].length === 0) {
+                        delete userSocketMap[userId];
+                        try {
+                            await User.findByIdAndUpdate(userId, { isOnline: false, lastSeen: new Date(), socketId: '' });
+                        } catch { }
+                    }
                 }
+                io.emit('onlineUsers', Object.keys(userSocketMap));
             }
-            io.emit('onlineUsers', Object.keys(userSocketMap));
         });
     });
 };
